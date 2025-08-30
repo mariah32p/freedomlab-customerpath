7fbe05d95b13d056dabc095274418ee273566888
mariah32p <mariah@32parallels.com>
2025-08-30 00:18:58 -0500

    Add signup route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import PricingPage from './pages/PricingPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
   )
